4111545f4c9d372760aacaedf9b9fa0ffadfbc5d
tangweilong <13698921186@139.com>
2019-07-13 10:53:55 +0800

    修改地图文件

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-
-import {Scene } from '@esri/react-arcgis';
-import BermudaTriangle from './BermudaTriangle';
+import MapControl  from './MapControl ';
 import Toggle from './Toggle';
 
 
 ReactDOM.render(
     <div id='r'>
-        <BermudaTriangle/>
+        <MapControl/>
         <Toggle/>
     </div>
     ,
