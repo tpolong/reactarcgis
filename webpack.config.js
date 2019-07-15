2f2f39452ed00a41572e52b3a381fe692e90774e
tangweilong <13698921186@139.com>
2019-07-15 12:19:42 +0800

    修改配置

--- webpack.config.js
+++ webpack.config.js
@@ -50,13 +50,8 @@ module.exports = function (_, arg) {
                 }
             ]
         },
-        // When importing a module whose path matches one of the following, just
-        // assume a corresponding global variable exists and use that instead.
-        // This is important because it allows us to avoid bundling all of our
-        // dependencies, which allows browsers to cache those libraries between builds.
         externals: [],
         plugins: [
-            // 想出多个页面就 new 多个htmlWebpackPlugin 及对应的文件位置 配置chunks确定引入什么模块
             new webpack.DefinePlugin({
                 "process.env.NODE_ENV": JSON.stringify(arg.mode || "production")
             }),
